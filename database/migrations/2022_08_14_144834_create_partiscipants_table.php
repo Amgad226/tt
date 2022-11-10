a45cbc278e6eee1d266a769d8bdd471e21678401
Amgad226 <amgad.wr.1@gmail.com>
2022-11-10 20:21:20 +0200

    d

--- database/migrations/2022_08_14_144834_create_partiscipants_table.php
+++ database/migrations/2022_08_14_144834_create_partiscipants_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->enum('role',['admin','member'])->default('member');
             
-            $table->timestamp('joined_at');
+            $table->timestamp('joined_at')->nullable();
             $table->primary(['conversation_id','user_id']);
         });
     }

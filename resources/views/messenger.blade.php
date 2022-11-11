c36aa6bdaabdfa63db8750006660330bbeb25023
Amgad226 <amgad.wr.1@gmail.com>
2022-11-11 16:22:19 +0200

    n

--- resources/views/messenger.blade.php
+++ resources/views/messenger.blade.php
@@ -6,7 +6,7 @@
         <head>
         <meta name="theme-color" content="#6777ef"/>
         <link rel="apple-touch-icon" href="{{ asset('img/logo.png') }}">
-        <link rel="manifest" href="{{ asset('/manifest.json') }}">
+        <link rel="manifest" href="{{ asset('manifest.json') }}">
 
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1, shrink-to-fit=no, viewport-fit=cover">

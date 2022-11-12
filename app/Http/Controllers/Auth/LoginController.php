9da9e6f08160fd09db1e660a3b20812b02c7e382
Amgad226 <amgad.wr.1@gmail.com>
2022-11-12 16:11:16 +0200

    n

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-use App\Http\Controllers\Auth\AuthenticatesUsers;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+// use App\Http\Controllers\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
@@ -26,7 +27,8 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo ='https://tt-226.herokuapp.com';
 
     /**
      * Create a new controller instance.

aebc509ed8addba5afdfeeb5e22a0844994993e8
Amgad226 <amgad.wr.1@gmail.com>
2022-11-12 13:43:29 +0200

    n

--- routes/web.php
+++ routes/web.php
@@ -43,9 +43,7 @@ Route::get('as',function(){
     return response()->json(['status'=>0,'message'=>'you shoud login']);
 })->name('jsonResponse');
 
-Route::group(['scheme' => 'https'], function () {
-    // Route::get(...)->name(...);
-});
+
 Route::view('/','messenger')->name('tt')->middleware('auth:sanctum');
 // Route::view('aa','messenger_copy');
 // Route::view('we','welcome');
